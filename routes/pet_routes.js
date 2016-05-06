eff2a471ef7ac9fc91ab44ebedd2d8e7c484bfe0
Robooto <samrussell63@gmail.com>
2016-05-05 22:18:03 -0700

    adding redis

--- routes/pet_routes.js
+++ routes/pet_routes.js
@@ -4,14 +4,62 @@ const r = require('request').defaults({
     json: true
 });
 
+const async = require('async');
+const redis = require('redis');
+
+const client = redis.createClient(6379, '127.0.0.1');
+
 module.exports = function(app) {
-    app.get('/pets', function(req, res) {
-        r({uri: 'http://localhost:3001/dog'}, function(error, response, body) {
-            if(!error && response.statusCode === 200) {
-                res.json(body);
+    app.get('/catname/:id', function(req, res) {
+        
+        client.get(req.params.id, function(error, cat) {
+            if (error) {throw error;}
+            if (cat) {
+                res.json(JSON.parse(cat));
             } else {
-                res.send(response.statusCode);
+                r({uri: 'http://localhost:3000/cat'}, function(error, response, body) {
+                    if (error) {throw error; }
+                    if (!error && response.statusCode === 200) {
+                        res.json(body);
+                        client.set(req.params.id, JSON.stringify(body), function(error) {
+                            if (error) {throw error;}
+                        });
+                    } else {
+                        res.send(response.statusCode);
+                    }
+                }); 
             }
+        });
+        
+    });
+    
+    app.get('/pets', function(req, res) {
+        
+        async.parallel({
+            cat: function (callback) {
+                r({uri: 'http://localhost:3000/cat'}, function(error, response, body) {
+                    if(!error && response.statusCode === 200) {
+                        callback(null, body.data);
+                    } else {
+                        callback(response.statusCode);
+                    }
+                });                
+            },
+            dog: function (callback) {
+                r({uri: 'http://localhost:3001/dog'}, function(error, response, body) {
+                    if(!error && response.statusCode === 200) {
+                        callback(null, body.data);
+                    } else {
+                        callback(response.statusCode);
+                    }
+                });                
+            }
+        },
+        function(error, results){
+            res.json({
+                error: error,
+                results: results
+            });
         });
     });
 };
